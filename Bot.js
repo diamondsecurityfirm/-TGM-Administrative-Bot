dfc31ee07de339fd146691d0ee60677b451082d0
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2018-07-20 12:44:23 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -63,7 +63,7 @@ var announcementchannel = channels.find("name", "announcements")
 	  announcementchannel.send({embed: {
     color: 3447003,
     author: {
-      name: message.author,
+      name: client.user.username,
       icon_url: client.user.avatarURL
     },
     title: "role-request",
