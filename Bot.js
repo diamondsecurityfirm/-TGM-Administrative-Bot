56c24d291beb7546d49f69279b5a81f90462d284
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2018-08-22 19:59:25 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const newUsers = new Discord.Collection();
-var prefix = '!';
 const swearWords = ["fuck", "flucking", "f/ck heads", "f/ck", "Niger", "f uck", "dick", "vagina", "pussy","nigger","asshole","bitch","bastard","queer","sex","slut","whore","jerk","jizz","cunt","crap","shit","shut up"];
 
 client.on('ready', () => {
@@ -23,6 +22,7 @@ function isCommand(command, message){
 let soruce = message
 let guild = message.guild
 let channels = guild.channels
+var prefix = '!';
 var ruleschannel = channels.find("name", "rules")
 
    if(isCommand('Test', message)){
