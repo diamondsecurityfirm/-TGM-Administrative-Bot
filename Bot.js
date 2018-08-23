bfaeb0009065502ac7419cf6d7cb7e44f0d626e7
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2018-08-22 19:46:05 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const newUsers = new Discord.Collection();
+var prefix = '!';
 const swearWords = ["fuck", "flucking", "f/ck heads", "f/ck", "Niger", "f uck", "dick", "vagina", "pussy","nigger","asshole","bitch","bastard","queer","sex","slut","whore","jerk","jizz","cunt","crap","shit","shut up"];
 
 client.on('ready', () => {
@@ -8,18 +9,28 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+function isCommand(command, message){
+	var command = command.toLowerCase();
+	var content = message.content.toLowerCase();
+	return content.startsWith(prefix + command);
+}
+
 // General commands
-client.on('message', message => {
+client.on('message', (message) => {
+if (message.author.bot) return; // Dont answer yourself.
+var args = message.content.split(/[ ]+/)
+   
 	
 let soruce = message
 let guild = message.guild
 let channels = guild.channels
-var prefix = '!';
 var ruleschannel = channels.find("name", "rules")
-var announcementchannel = channels.find("name", "announcements")
-var oneplatoon = channels.find("name", "1st-company-announcements")
 
-
+   if(isCommand('Test', message)){
+    	message.reply('Test has passed, new command system is up and working ;) ');
+    }
+});
+	
 if ( swearWords.some(word => message.content.toLowerCase().includes(word)) ) {
  message.delete();
  message.reply("Swearing is against [TGM] discord rules.");
