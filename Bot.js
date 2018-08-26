5847d21ed772fadbc234b720320e2b052e2eeec7
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2018-08-26 01:49:13 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -41,14 +41,31 @@ var ruleschannel = channels.find("name", "rules")
     		message.channel.send("Please enter a username.")
     	}
     	return;
-    } else if(isCommand('Ban', message)){
-    let modRole = message.guild.roles.find("name", "HQ");
-    if(message.member.roles.has(modRole.id)) { 
-      let banMember = message.guild.member(message.mentions.users.first());
-      message.guild.member(banMember).ban();
-      message.channel.sendMessage("Member banned.");
-    } else {
-      message.reply("Your permission levels prevent you from using this command!");
-    }
+    } else if(isCommand('Kick', message)){
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "admin-logs");
+    if(!kickChannel) return message.channel.send("Can't find admin-logs channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+	    
+    return;
+  }
+	
+});
 
 client.login(process.env.BOT_TOKEN);
