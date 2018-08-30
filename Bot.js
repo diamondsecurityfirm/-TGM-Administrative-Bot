de7b6b2f527ce59c0a0251a93fd88cc6385a1695
diamondsecurityfirm <41444551+diamondsecurityfirm@users.noreply.github.com>
2018-08-29 20:35:18 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -100,7 +100,6 @@ var logchannel = channels.find("name", "admin-logs")
 	    
     return;
   }
-});
 
 client.on('message', (message) => {
 if (message.author.bot) return; // Dont answer yourself.
@@ -117,7 +116,5 @@ if(isCommand('Purge', message)){
   if(!args[0]) return message.channel.send("You must enter a number of messages to delete!");
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send("Deleted ${args[0]} messages.").then(msg => msg.delete(5000));
-  }
-
 	    
 client.login(process.env.BOT_TOKEN);
